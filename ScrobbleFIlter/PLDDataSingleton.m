6c56b30295bb34c76fd1a54f6710f5b0c53b7006
Phillip Dunham <pdunham@nextag.com>
2012-06-03 15:27:45 -0500

    commented the singleton class

--- ScrobbleFIlter/PLDDataSingleton.m
+++ ScrobbleFIlter/PLDDataSingleton.m
@@ -5,10 +5,18 @@
 //  Created by Phillip Dunham on 5/21/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
+//  This class handles all most of the data for the application.
+//  As you can probably tell, the idea for the class came when the original intent was to store the data in CoreData.
+//  However, all the data that needs to be stored for the app so far can be stored in plists if not directly in user defaults
+//  This class also handled downloading and holding the list of recent scrobbles from last.fm.  Since more than one ViewController is interested in using that list,
+//  namely the PLDTweetViewController and the PLDRecentScrobblesViewCOntroller, and it is important for them to be working off of the same list,
+//  it makes sense to centralize that in this singleton class so that they are in agreement.
+//
 
 #import "PLDDataSingleton.h"
 
 @implementation PLDDataSingleton
+
 
 @synthesize scrobbledArtists, filteredArtists, isLastFmNameValid, downloadFailed;
 
@@ -26,6 +34,11 @@
     return _sharedObject;
 }
 
+/*******************************************************************************
+ * @method loadlastfmname  
+ * @abstract loads the user's last.fm name from user defaults   
+ * @description  Also sets boolen flag if the name is nil 
+ *******************************************************************************/
 -(NSString *) loadlastfmname {
     NSUserDefaults *settings = [NSUserDefaults standardUserDefaults];
     NSString *lastfmname = [settings objectForKey:@"lastfmname"];
@@ -33,6 +46,12 @@
     return lastfmname;
 }
 
+/*******************************************************************************
+ * @method loadScrobbles  
+ * @abstract downloads list of recent scrobbles from last.fm   
+ * @description  uses last.fm's RESTful API to download top artists from the last 7 days. WIll post notifications when successful or if an error occured 
+ *      once loaded the list is kept in a property that is accessible to other classes
+ *******************************************************************************/
 -(void)loadScrobbles {
     NSLog(@"loading scrobbles");
     NSString *lastFmName = [self loadlastfmname];
@@ -67,7 +86,12 @@
     
 }
 
-
+/*******************************************************************************
+ * @method loadFilteredArtists  
+ * @abstract loads list of fiiltered artists from file   
+ * @description  The list of filtered artists is kept in a plist in the same directory as user default. 
+ *      once loaded the list is kept in a property that is accessible to other classes
+ *******************************************************************************/
 -(void) loadFilteredArtists {
     NSString *path = [[NSBundle mainBundle] bundlePath];
     NSString *finalPath = [path stringByAppendingPathComponent:@"filteredartists.plist"];
@@ -81,6 +105,12 @@
     filteredArtists = [NSMutableSet setWithArray:filteredArtistsArray];
 }
 
+/*******************************************************************************
+ * @method soterFilteredArtists  
+ * @abstract writes list of filtered artists to file   
+ * @description  The list of filtered artists, which can be set as a [property on this singleton, 
+ *          is written tot a plist file in the same directory as user defaults
+ *******************************************************************************/
 -(void) storeFilteredArtists {
     BOOL success;
     NSError *error;
@@ -109,6 +139,12 @@
     
 }
 
+/*******************************************************************************
+ * @method filterScrobbles  
+ * @abstract returns a list of recent scrobbles minus any artists in the filteredArtists list   
+ * @description  The singleton keeps two arrays - the list of recent scrobbled artists (which is unfiltered) and the list of artists to filter
+ *      When the RecentScrobblesVIewController wants a list to display, it calls this method  
+ *******************************************************************************/
 -(NSArray *) filterScrobbles {
     
     NSMutableArray * filteredList =  [[NSMutableArray alloc] initWithArray:scrobbledArtists copyItems:YES]; 
@@ -123,6 +159,12 @@
     return filteredList;
 }
 
+/*******************************************************************************
+ * @method topThree  
+ * @abstract returns an array with the top three recent scrobbles   
+ * @description  This method returns the top three elements from the filtered list (from the filterScrobbles method)
+ *      This is used y the TweetViewController
+ *******************************************************************************/
 -(NSArray *) topThree   {
     
     NSArray *filteredList = [self filterScrobbles];
